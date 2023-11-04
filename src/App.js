d261cb4cd97759415960839193a121e3fadfb92d
kuridee <kuridee1081993@gmail.com>
2023-11-05 00:51:03 +0800

    f

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p id='text'>Christian </p>
+      <p id='text'>Cadiz </p>
+    </div>
+  );
+}
+
+export default App;
